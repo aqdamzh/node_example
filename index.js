3570e92905d2263d725dd18be8e0396b391fa867
aqdamzh <aqdamzain1@gmail.com>
2023-07-10 20:32:23 +0700

    add some pages

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('This is the index page');
+})
+
+app.get('/about', (req, res) => {
+    res.send('This is the about page');
+})
+
+app.use((req, res) => {
+    res.status(404).send('Page not found');
 })
 
 app.listen(port, () => {
