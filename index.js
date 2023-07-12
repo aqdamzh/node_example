b188208a35037483da930049e28630f4a8de3491
aqdamzh <aqdamzain1@gmail.com>
2023-07-12 19:40:28 +0700

    add example dynamic path with param

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({extended: false}));
+
 app.get('/', (req, res) => {
   res.send('This is the index page');
 })
@@ -10,8 +12,15 @@ app.get('/about', (req, res) => {
     res.send('This is the about page');
 })
 
+app.get('/users/:email', (req, res) => {
+  res.send(`This user email is ${req.params.email}`);
+})
+
+
+
+
 app.use((req, res) => {
-    res.status(404).send('Page not found');
+  res.status(404).send('Page not found');
 })
 
 app.listen(port, () => {
