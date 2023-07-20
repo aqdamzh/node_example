6110e3168764740c3e45f962cb2e057373b753dc
aqdamzh <aqdamzain1@gmail.com>
2023-07-20 21:08:02 +0700

    API playlist service -> unit test sort most played (descending)

--- API/__tests__/playlistService.test.js
+++ API/__tests__/playlistService.test.js
@@ -6,3 +6,16 @@ test('updated play count, It should update play count in song', () => {
     const updatedPlayCount = PlaylistService.incPlayCount(1).playCount;
     expect(updatedPlayCount).toEqual(oldPlayCount + 1);
 });
+
+//--------------------------------------------------------------
+test('most played song, It should sort playlist descending by play count', () => {
+    const mostPlayedSongs = PlaylistService.getMostPlayed();
+
+    //check if it is already sorted descending properly
+    for(let i=0; i<mostPlayedSongs.length; i++){
+        for(let j = i+1; j<mostPlayedSongs.length; j++){
+            expect(mostPlayedSongs[i].playCount > mostPlayedSongs[j].playCount)
+            .toBe(true);
+        }
+    }
+});
